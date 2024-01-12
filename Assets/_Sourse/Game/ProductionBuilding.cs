b6611f0946e3b108101e355b07ebe8c8bbed48df
Daniil Makagonov <daniil1makagonov@gmail.com>
2024-01-12 20:56:38 +0300

    feat(button): Button for increase resources
    Button for increasing resources with supporting function

---
+++ Assets/_Sourse/Game/ProductionBuilding.cs
@@ -0,0 +1,18 @@
+﻿using Core;
+using UnityEngine;
+
+namespace Game
+{
+  public class ProductionBuilding : MonoBehaviour
+  {
+    [SerializeField] 
+    private GameManager _gameManager;
+    [SerializeField] 
+    private GameResource _resource;
+
+    public void Increase()
+    {
+      _gameManager.ResourceBank.ChangeResource(_resource, 1);
+    }
+  }
+}
